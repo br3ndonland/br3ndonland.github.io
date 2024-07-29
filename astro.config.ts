f3463eca9cd4608b3c964d946450474040fedb23
Brendon Smith <bws@bws.bio>
2024-07-29 14:17:20 -0400

    Add Astro integration to autolink HTML headings
    Commit aeb2ab8 added a Rehype plugin to automatically add anchor links
    to headings from Markdown and MDX files. This heading autolinking works
    well for Markdown, but does not process headings in non-Markdown files.
    This commit will implement heading autolinking for non-Markdown files.
    
    To accomplish this, an Astro integration will be added. This integration
    will use the `astro:build:done` lifecycle hook to process files after
    they have been built into HTML. The integration will accept a list of
    file paths to transform. For each file, the integration will process the
    HTML using the same Rehype plugin and configuration used for Markdown.
    The result will be that heading autolinking will be performed on
    Markdown and non-Markdown files in the same consistent way.
    
    The integration currently only operates on the `astro:build:done` hook.
    This means that heading autolinking will not be applied when starting
    the Vite development server. In the future, additional build hooks could
    be used to apply heading autolinking when running in development mode.
    
    https://docs.astro.build/en/reference/integrations-reference/
    https://github.com/rehypejs/rehype-autolink-headings
    https://vitejs.dev/guide/api-plugin.html
    https://rollupjs.org/plugin-development/

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,41 @@
+import { type AstroIntegration } from "astro"
 import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import { rehypeHeadingIds } from "@astrojs/markdown-remark"
+import { readFile, writeFile } from "node:fs/promises"
+import { rehype } from "rehype"
 import type { Options as RehypeAutolinkOptions } from "rehype-autolink-headings"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
+
+interface AstroAutolinkOptions {
+  paths: string[]
+  rehypeAutolinkOptions?: Readonly<RehypeAutolinkOptions> | null | undefined
+}
+
+const astroAutolinkHeadings = (
+  astroAutolinkOptions: AstroAutolinkOptions,
+): AstroIntegration => {
+  const { paths, rehypeAutolinkOptions } = astroAutolinkOptions
+  const integrationName = "astro-autolink-headings"
+  return {
+    name: integrationName,
+    hooks: {
+      "astro:build:done": async (options) => {
+        const logger = options.logger.fork(`${integrationName}/build`)
+        paths.forEach(async (path) => {
+          logger.info(`Processing ${path}`)
+          const fileContents = await readFile(path)
+          const processedFileContents = await rehype()
+            .use(rehypeHeadingIds)
+            .use(rehypeAutolinkHeadings, rehypeAutolinkOptions)
+            .process(fileContents)
+          await writeFile(path, String(processedFileContents))
+        })
+      },
+    },
+  }
+}
 
 const rehypeAutolinkOptions: RehypeAutolinkOptions = {
   behavior: "prepend",
@@ -17,10 +49,16 @@ const rehypeAutolinkOptions: RehypeAutolinkOptions = {
   },
   headingProperties: { tabIndex: "-1", className: ["heading-element"] },
   properties: { ariaLabel: "Link to self", className: ["anchor-link"] },
+  test: ["h2", "h3", "h4", "h5", "h6"],
+}
+
+const astroAutolinkOptions: AstroAutolinkOptions = {
+  paths: ["./dist/about/index.html"],
+  rehypeAutolinkOptions: rehypeAutolinkOptions,
 }
 
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
+  integrations: [astroAutolinkHeadings(astroAutolinkOptions), mdx(), sitemap()],
   markdown: {
     rehypePlugins: [
       rehypeHeadingIds,
