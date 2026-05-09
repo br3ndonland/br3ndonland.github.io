dc874c72fe3d6f6ef5f34a6156cf426ca5b88a3e
Brendon Smith <bws@bws.bio>
2026-05-08 21:28:10 -0400

    Reorder test assertions

--- tests/unit/lightbox-options.test.ts
+++ tests/unit/lightbox-options.test.ts
@@ -10,10 +10,10 @@ describe("GLightbox options", () => {
   // https://github.com/biati-digital/glightbox/blob/a9385e5/src/js/glightbox.js#L54-L55
   it("prevents GLightbox from loading separate plyr assets from CDN", () => {
     const cdn_domain = "cdn.plyr.io"
-    expect(GLIGHTBOX_OPTIONS.plyr.config.blankVideo).not.toContain(cdn_domain)
     expect(GLIGHTBOX_OPTIONS.plyr.config.blankVideo).toBeDefined()
-    expect(GLIGHTBOX_OPTIONS.plyr.config.iconUrl).not.toContain(cdn_domain)
+    expect(GLIGHTBOX_OPTIONS.plyr.config.blankVideo).not.toContain(cdn_domain)
     expect(GLIGHTBOX_OPTIONS.plyr.config.iconUrl).toBeDefined()
+    expect(GLIGHTBOX_OPTIONS.plyr.config.iconUrl).not.toContain(cdn_domain)
     expect(GLIGHTBOX_OPTIONS.plyr.css).toBeDefined()
     expect(GLIGHTBOX_OPTIONS.plyr.js).toBeDefined()
   })
